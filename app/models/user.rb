6234dc0ae7307d26ddf8206be157e964795db7e8
akane <h1p62est@gmail.com>
2020-09-23 13:14:13 +0000

    rails04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   #名前を32文字まで
   validates :name, presence: true, length: { maximum:32}
   #emailの正規表現を追加
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\Z/i }
   #パスワードの文字制限8-32まで
-  validates :password, presence: true, length: { in: 8..32}
+  validates :password, length: { in: 8..32}
   #パスワードをアルファベット、数字の混合のみ可能
-  validates :password, presence: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
   has_secure_password
 end
 
