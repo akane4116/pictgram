7aabe9a20f74eb0ffb62fc421742436e51498d4e
akane <h1p62est@gmail.com>
2020-10-09 04:41:45 +0000

    Rails05再考

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,12 +7,20 @@ class FavoritesController < ApplicationController
   favorite = Favorite.new
   favorite.user_id = current_user.id
   favorite.topic_id = params[:topic_id]
-  
+
+   
    if favorite.save
     redirect_to topics_path, success: 'お気に入りに登録しました'
    else
     redirect_to topics_path, danger: 'お気に入りの登録に失敗しました'
    end
   end
+  
+    def show
+   @topic= Topic.find_by(id_params[:id])
+   @user = @topic_user
+   @favorites_count = Favorite.where(topic_id: @topic.id).count
+    end
+   
 end
 
