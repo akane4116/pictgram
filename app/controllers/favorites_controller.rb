5a89b0943044542429539da2152f4ed65ad717cd
akane <h1p62est@gmail.com>
2020-10-11 13:43:19 +0000

    Rails07 課題まで

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -15,5 +15,14 @@ class FavoritesController < ApplicationController
     redirect_to topics_path, danger: 'お気に入りの登録に失敗しました'
    end
   end
+  
+  def destroy
+   @favorite_topics = current_user.favorites.find_by(topic_id:params[:topic_id])
+    if @favorite_topics.destroy
+     redirect_to topics_path, success: 'お気に入りを解除しました'
+    end
+  end
+  
+  
 end
 
