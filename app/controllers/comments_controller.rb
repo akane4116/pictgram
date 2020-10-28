c1c08f6015b67b3fe2c30cbf29f5c07907739a9e
akane <h1p62est@gmail.com>
2020-10-28 04:46:42 +0000

    10/28

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,8 @@ class CommentsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @comment = @topic.comments.new(comment_params)
     @comment.user_id = current_user.id
-    @comment.topic_id = params[:topic_id]
     @comment.content = params[:comment][:content]
     
-    binding.pry
     if @comment.save
       flash[:success] = 'コメントしました'
       redirect_to topics_path
